0415f656d21e3a56af4036cb8be363093c4a0d30
大坪 結子 <yuikoots@fsi.co.jp>
2019-04-11 16:05:40 +0900

    progress個別

---
+++ pro/sw2.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'hiyoko';
+const urlsToCache = [
+	'./index.html',
+	'./chikuwa-hiyoko.png'
+];
+
+//登録処理
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+	caches.open(CACHE_NAME)
+	  .then((cache) => {
+		  // 指定されたリソースをキャッシュに追加する
+		  return cache.addAll(urlsToCache);
+	  })
+	);
+});
+
+self.addEventListener('activate', (event) => {
+var cacheWhitelist = [CACHE_NAME];
+event.waitUntil(
+	caches.keys().then((cacheNames) => {
+		return Promise.all(
+			cacheNames.map((cacheName) => {
+				// 古いキャッシュは削除する
+				if (cacheWhitelist.indexOf(cacheName) === -1) {
+					return caches.delete(cacheName);
+				}
+			})
+			);
+		})
+	);
+});
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+	caches.match(event.request)
+	  .then((response) => {
+		  if (response) {
+		  	return response;
+		  }
+		let fetchRequest = event.request.clone();
+		return fetch(fetchRequest).then((response) => {
+			if (!response || response.status !== 200 || response.type !== 'basic') {
+				return response;
+			}
+			let responseToCache = response.clone();
+			caches.open(CACHE_NAME).then((cache) => {
+				cache.put(event.request, responseToCache);
+			});
+			return response;
+			});
+		})
+	);
+});
